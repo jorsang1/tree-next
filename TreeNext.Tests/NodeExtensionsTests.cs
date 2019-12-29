9cb3b5f0ce7b01f9a5af6d3289b73ecc694c143c
NOVA\JS101 <jordi.sanchez@novasol.com>
2019-12-29 12:12:04 +0100

    Test solution with a complete tree (green already)

--- TreeNext.Tests/NodeExtensionsTests.cs
+++ TreeNext.Tests/NodeExtensionsTests.cs
@@ -89,5 +89,46 @@ namespace TreeNext.Tests
             result = result.Next();
             Assert.AreEqual(result.Data, 6);
         }
+
+        [TestMethod]
+        public void Next_WHEN_CompleteTreeWithDifferentLevels_THEN_exploreInDeepFirstAndGetNullAtTheEnd()
+        {
+            var sut = new Node(1,
+                new Node(2,
+                    new Node(3),
+                    new Node(4,
+                        new Node(41),
+                        new Node(42)),
+                    new Node(5)),
+                new Node(6),
+                new Node(7));
+
+            var result = sut.Next();
+            Assert.AreEqual(result.Data, 2);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 3);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 4);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 41);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 42);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 5);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 6);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 7);
+
+            result = result.Next();
+            Assert.IsNull(result);
+        }
     }
 }
