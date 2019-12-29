de7cfafc1e4304a2a9c58d76de6f81b78775607c
NOVA\JS101 <jordi.sanchez@novasol.com>
2019-12-29 11:24:41 +0100

    Only children scenario (green already)

--- TreeNext.Tests/NodeExtensionsTests.cs
+++ TreeNext.Tests/NodeExtensionsTests.cs
@@ -24,5 +24,23 @@ namespace TreeNext.Tests
 
             Assert.AreEqual(result.Data, 2);
         }
+
+        [TestMethod]
+        public void Next_WHEN_TreeHasDifferentChildren_THEN_returnAllChildrenInDeep()
+        {
+            var sut = new Node(1,
+                new Node(2,
+                    new Node(3,
+                        new Node(4))));
+
+            var result = sut.Next();
+            Assert.AreEqual(result.Data, 2);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 3);
+
+            result = result.Next();
+            Assert.AreEqual(result.Data, 4);
+        }
     }
 }
